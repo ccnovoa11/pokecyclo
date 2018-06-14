fdedad1dba40a53506cc4c0f7c5afe61e7296c1a
ccnovoa11 <cc.novoa11@uniandes.edu.co>
2018-06-14 15:50:38 -0500

    optional logos

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="https://raw.githubusercontent.com/ccnovoa11/pokecyclo/master/src/logo.png" className="App-logo" alt="logo" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
